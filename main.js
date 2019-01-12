02ce9f300d0d9110376a3a2749231c9198af997b
genilton <genilton.souza26@gmail.com>
2019-01-12 00:34:12 -0400

    cap 02
    Initial commit

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 'use strict';
 
-//electron imports
+//load electron 
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
+
 let mainWindow = null;
 
-//app event listener
+//close event listener
 app.on('window-all-closed', () => {
-    //quit when all windows are closed, except for macOSX
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-//create new window
 app.on('ready', () => {
     mainWindow = new BrowserWindow();
     mainWindow.loadURL(path.join(__dirname, 'index.html'));
-    mainWindow.on('closed', () => {
+    mainWindow.on('close', () => {
         mainWindow = null;
     });
-
 });
