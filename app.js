34b72cf824816a3b4626c9b7513cfecfa7fb9803
genilton <genilton.souza26@gmail.com>
2019-01-15 00:10:26 -0400

    finished session 3.1.2

--- app.js
+++ app.js
@@ -1,76 +1,18 @@
 'use strict';
+const fileSystem = require('./fileSystem');
+const userInterface = require('./userInterface');
 
-const async = require('async');
-const osenv = require('osenv');
-const fs = require('fs');
-const path = require('path');
 
-function getHomeUserFolder() {
-    return osenv.home();
+function main(){
+    userInterface.bindDocument(window);
+    let folderPath = fileSystem.getUsersHomeFolder();
+    // fileSystem.getFilesInFolder(folderPath,(err,files)=>{
+    //     if(err){
+    //         return alert('Sorry, you could not load your home folder');
+    //     }
+    //     fileSystem.inspectAndDescribeFiles(folderPath,files,userInterface.displayFiles);
+    // });
+    userInterface.loadDirectory(folderPath)(window);
 }
 
-function getFilesInFolder(folderPath, cb) {
-    fs.readdir(folderPath, cb); // cb-> call back
-}
-
-function inspectAndDescribeFile(filePath, cb) { //use the path module to get name for file
-    let result = {
-        file: path.basename(filePath),
-        path: filePath,
-        type: ''
-    };
-
-    //query object to find what type is it;
-    fs.stat(filePath, (err, stat) => {
-        if (err) {
-            cb(err);
-        } else {
-            if (stat.isFile()) {
-                result.type = 'file';
-            }
-            if (stat.isDirectory()) {
-                result.type = 'directory';
-            }
-            cb(err, result);
-        }
-    });
-}
-
-function inspectAndDescribeFiles(folderPath, files, cb) {
-    async.map(files, (file, asyncCb) => {
-
-        //call asynchronous function 
-        let resolvedFilePath = path.resolve(folderPath, file);
-        inspectAndDescribeFile(resolvedFilePath, asyncCb);
-    }, cb);
-}
-
-function displayFile(file) {
-    const mainArea = document.getElementById('main-area');
-    const template = document.querySelector('#item-template');
-    let clone = document.importNode(template.content, true);
-    clone.querySelector('img').src = `images/${file.type}.svg`;
-    clone.querySelector('.filename').innerText = file.file;
-
-    mainArea.appendChild(clone);
-}
-
-
-function displayFiles(err, files) {
-    if (err) {
-        return alert('Sorry,could not display your files');
-    }
-    files.forEach(displayFile);
-}
-
-function main() {
-    const folderPath = getHomeUserFolder();
-    getFilesInFolder(folderPath, (err, files) => {
-        if (err) {
-            return alert("Sorry, could not load your home folder");
-        }
-        inspectAndDescribeFiles(folderPath, files, displayFiles);
-    });
-}
-
-main();
+window.onload = main;
