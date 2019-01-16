39a14102e22cf58de58e6dbb3f66b04d65fee4b7
genilton <genilton.souza26@gmail.com>
2019-01-16 01:04:34 -0400

    Downgrade lunr to version 0.7

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ const search = require('./Search');
 function main() {
     userInterface.bindDocument(window);
     let folderPath = fileSystem.getUsersHomeFolder();
-    // fileSystem.getFilesInFolder(folderPath,(err,files)=>{
-    //     if(err){
-    //         return alert('Sorry, you could not load your home folder');
-    //     }
-    //     fileSystem.inspectAndDescribeFiles(folderPath,files,userInterface.displayFiles);
-    // });
     userInterface.loadDirectory(folderPath)(window);
     userInterface.bindSearchField((event) => {
         const query = event.target.value;
